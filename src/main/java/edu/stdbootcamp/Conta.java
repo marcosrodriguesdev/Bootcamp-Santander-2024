8dfdbd2fdbf926a531d373116d94dd7a47b07b23
marcosrodrigues <marcosrodrigues.sistemas@gmail.com>
2024-05-20 12:56:31 -0300

    Correção de nome de pacote

---
+++ src/main/java/edu/stdbootcamp/Conta.java
@@ -0,0 +1,40 @@
+package edu.stdbootcamp;
+
+public class Conta {
+    private int numero;
+    private String agencia;
+    private String nomeCliente;
+    private Double saldo;
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getAgencia() {
+        return agencia;
+    }
+
+    public void setAgencia(String agencia) {
+        this.agencia = agencia;
+    }
+
+    public String getNomeCliente() {
+        return nomeCliente;
+    }
+
+    public void setNomeCliente(String nomeCliente) {
+        this.nomeCliente = nomeCliente;
+    }
+
+    public Double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(Double saldo) {
+        this.saldo = saldo;
+    }
+}
---
+++ src/main/java/edu/stdbootcamp/ContaTerminal.java
@@ -0,0 +1,21 @@
+package edu.stdbootcamp;
+
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args) {
+        Conta conta = new Conta();
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite o número da Agência: ");
+        conta.setAgencia(sc.next());
+        System.out.println("Digite o Numero da Conta: ");
+        conta.setNumero(sc.nextInt());
+        System.out.println("Digite o nome do Cliente: ");
+        conta.setNomeCliente(sc.next());
+        System.out.println("Digite o saldo da Conta: ");
+        conta.setSaldo(sc.nextDouble());
+
+        System.out.printf("Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %s e seu saldo %s já está disponível para saque%n",conta.getNomeCliente(), conta.getAgencia(), conta.getNumero(), conta.getSaldo());
+    }
+}
